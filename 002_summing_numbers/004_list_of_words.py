d38312b453d6987e1cce86c4bf4042458e0d4512
Ernest <ejmurray247@hotmail.com>
2020-12-15 12:39:22 +0000

    Updated filtering of strings.

--- 002_summing_numbers/004_list_of_words.py
+++ 002_summing_numbers/004_list_of_words.py
@@ -1,15 +1,15 @@
 def list_of_words(words):
-    """Print the length of the longest, shortest and average word length.
+    """Print the length of the shortest, longest and average word length.
 
     Returns:
-        [tuple]: (longest word, shortest word, average word length)
+        [tuple]: (shortest word, longest word, average word length)
     """
 
     word_list_lengths = sorted([len(word) for word in words])
     print(f"Word list length: {word_list_lengths}")
     print(f"Shortest word: {min(word_list_lengths)}")
     print(f"Longest word: {max(word_list_lengths)}")
-    return max(word_list_lengths), min(word_list_lengths), sum(word_list_lengths) / len(words)
+    return min(word_list_lengths), max(word_list_lengths), sum(word_list_lengths) / len(words)
 
 
 print(
