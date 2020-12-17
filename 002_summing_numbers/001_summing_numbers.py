ff3452b386ebf6578ee97724fb54096c96589667
Ernest <ejmurray247@hotmail.com>
2020-12-17 09:41:10 +0000

    Updated folder structure Added test folder

--- 002_summing_numbers/001_summing_numbers.py
+++
@@ -1,8 +0,0 @@
-def my_sum(*numbers):
-    start_num = 0
-    for i in numbers:
-        start_num += i
-    return start_num
-
-
-print(my_sum(*[10, 20, 30, 40, 50]))
---
+++ chapter_001/tests/test_e001_guess_the _number.py
@@ -0,0 +1,13 @@
+from io import StringIO
+import random
+import pytest
+from chapter_001 import c001_numeric_types
+
+
+def test_correct(monkeypatch, capsys):
+    random.seed(0)              # first randint will be 100
+    monkeypatch.setattr('sys.stdin', StringIO('49\n'))
+     guess_the_number()
+    captured_out, captured_err = capsys.readouterr()
+
+    assert captured_out.endswith('Just right! I had the number 49 in mind.')
